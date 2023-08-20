7d3dd5b5e967ca080133553d1d4aaee544c13e61
yuzu-krs <yztimai@icloud.com>
2023-08-20 12:51:37 +0900

    txtファイルの末尾の文字を認識するようにし，数字+.+数字の形のみ.が許されるように改良した．次は，アポストロフィーの機能追加が必要．

--- command.c
+++ command.c
@@ -1,10 +1,19 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-int isAlphabetNum(int c) {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
-           (c >= '0' && c <= '9');
+
+int isApostrophe(int c) { return (c == '\''); }
+
+int isPeriod(int c) { return (c == '.'); }
+
+int isNum(int c) { return (c >= '0' && c <= '9'); }
+
+int isAlphabet(int c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
+
+int isAlphabetNum(int c) { return isAlphabet(c) || isNum(c); }
 
 int main(int argc, char* argv[]) {
     char inputWord[1024];
@@ -24,21 +33,37 @@ int main(int argc, char* argv[]) {
     int c;
     int i = 0;
     int lineNumber = 1;
+
     while ((c = fgetc(inputFile)) != EOF) {
-        // 1単語として取り出す．
-        // 大文字を小文字に変換する
-        if (isAlphabetNum(c)) {
-            inputWord[i] = tolower(c);
-            i++;
-        } else if (i != 0) {
-            inputWord[i] = '\0';
+        // 数字以外+Apostrophe+数字以外ならば無視
+        if (isPeriod(inputWord[i - 1]) && !(isNum(c)) && i > 1) {
+            inputWord[i - 1] = '\0';
             printf("行番号:%d,inputWord:%s \n", lineNumber, inputWord);
             i = 0;
         }
-        // 改行があれば，+1
+
+        // 1単語として取り出す
+        // 大文字を小文字に変換する
+        if (isAlphabetNum(c) || isApostrophe(c) || (i > 0 && isPeriod(c))) {
+            inputWord[i] = tolower(c);
+            i++;
+        } else if ((i != 0) && !(isPeriod(inputWord[i - 1]))) {
+            inputWord[i] = '\0';
+            printf("行番号:%d,inputWord:%s \n", lineNumber, inputWord);
+            i = 0;
+        } else {
+            i = 0;
+        }
         if (c == '\n') {
+            i = 0;
             lineNumber++;
         }
+    }
+
+    // ファイルの末尾に単語があった場合
+    if (i != 0) {
+        inputWord[i] = '\0';
+        printf("行番号:%d,inputWord:%s \n", lineNumber, inputWord);
     }
 
     fclose(inputFile);
